7e00590f052e2db67b289f277c095688ebd9f33c
exetch <gassemark@gmail.com>
2023-05-26 21:20:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,24 +6,23 @@ from utils import load_candidates, get_by_pk, get_by_skill, update_candidate, \
 app = Flask(__name__)
 
 
-# Главная страница
+
 @app.route('/')
 def index():
     candidates = load_candidates()
     return render_template('index.html', candidates=candidates)
 
 
-# Страница кандидата по pk
+
 @app.route('/candidate/<int:pk>')
 def candidate_details(pk):
-    candidate = get_by_pk(pk)  # Получение данных кандидата по pk
+    candidate = get_by_pk(pk)
     return render_template('candidate.html', candidate=candidate)
 
 
-# Страница поиска по навыку
 @app.route('/skills/<skill>')
 def skill_search(skill):
-    candidates = get_by_skill(skill)  # Получение списка кандидатов по навыку
+    candidates = get_by_skill(skill)
     return render_template('skill_search.html', candidates=candidates)
 
 
